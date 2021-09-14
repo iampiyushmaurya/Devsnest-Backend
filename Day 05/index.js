8a2787b95e505d01c750c56d6c3795c3db6f24ad
Piyush Maurya <55586404+iampiyushmaurya@users.noreply.github.com>
2021-09-14 16:01:16 +0530

    Add files via upload

---
+++ Day 05/index.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+// fs.mkdirSync("folder_1");
+// fs.writeFileSync("folder_1/text.txt", "hello");
+// // const data = fs.readFileSync("folder_1/text.txt");
+// // console.log(data);
+// fs.appendFileSync("folder_1/text.txt", " world");
+// const newdata = fs.readFileSync("folder_1/text.txt", "utf-8")
+// console.log(newdata);
+// fs.renameSync('folder_1/text.txt', 'folder_1/textnew.txt');
+// fs.unlinkSync('folder_1/textnew.txt');
+fs.rmdirSync('folder_1');
+
+
